20e102afc4c6a34b943ed9e8d8c30b5bc8372367
Giorgi Chavchanidze <giorgi@vivaldi.com>
2021-12-31 07:28:01 +0100

    Key rollover and standalone modifiers

--- src/vkbd/usb-gadget.c
+++ src/vkbd/usb-gadget.c
@@ -8,6 +8,14 @@
 #include "usb-gadget.h"
 
 uint16_t mods = 0;
+uint8_t keys[] = {
+	0,
+	0,
+	0,
+	0,
+	0,
+	0
+};
 
 struct vkbd {
 	int fd;
@@ -15,8 +23,7 @@ struct vkbd {
 
 struct hid_report {
 	uint16_t hid_mods;
-	uint16_t hid_code;
-	uint32_t fill;
+	uint8_t hid_code[6];
 };
 
 static int create_virtual_keyboard(void)
@@ -47,14 +54,15 @@ uint16_t hid_code(uint16_t code)
 	return 0;
 }
 
-void send_hid_report (const struct vkbd *vkbd, uint16_t code, uint16_t mods)
+void send_hid_report (const struct vkbd *vkbd)
 {
 
 	struct hid_report report;
 
-	report.hid_code = hid_code(code);
+	for (int i = 0; i < 6; i++) {
+		report.hid_code[i] = hid_code(keys[i]);
+	}
 	report.hid_mods = mods;
-	report.fill = 0;
 
 	write(vkbd->fd,&report,sizeof(report));
 
@@ -110,16 +118,34 @@ static int set_modifier_state(int code, int state)
 
 }
 
+void set_key_state(int code, int state)
+{
+	int set = 0;
+
+	for (int i = 0; i < 6; i++) {
+		if(keys[i] == code) {
+			set = 1;
+			if(state == 0) {
+				keys[i] = 0;
+			}
+		}
+	}
+	if(!set) {
+		for (int i = 0; i < 6; i++) {
+			if(keys[i] == 0 && state && !set) {
+				set = 1;
+				keys[i] = code;
+			}
+		}
+	}
+}
+
 
 void vkbd_send(const struct vkbd *vkbd, int code, int state)
 {
-	if(!set_modifier_state(code, state)) {
-		if(state ) {
-			send_hid_report(vkbd, code, mods);
-		} else {
-			send_hid_report(vkbd, 0, 0);
-		}
-	}
+	set_modifier_state(code, state);
+	set_key_state(code, state);
+	send_hid_report(vkbd);
 }
 
 void vkbd_move_mouse(const struct vkbd *vkbd, int x, int y)
