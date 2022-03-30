rvaiya/keyd

commit a1edfab5d124aa70493dd988973f17bf4a9a4c30

refactor: cleanup project structure + trim makefile

Makefile
@@ -1,15 +1,16 @@
-.PHONY: all clean install install-usb-gadget uninstall uninstall-usb-gadget debug man
+.PHONY: all clean install uninstall debug assets
 DESTDIR=
 PREFIX=/usr
 
 VERSION=2.3.0-rc
 COMMIT=$(shell git describe --no-match --always --abbrev=7 --dirty)
+VKBD=uinput
 
 CFLAGS+=-DVERSION=\"v$(VERSION)\ \($(COMMIT)\)\" \
 	-I/usr/local/include \
 	-L/usr/local/lib
 
-platform+=$(shell uname -s)
+platform=$(shell uname -s)
 
 ifeq ($(platform), Linux)
 	COMPAT_FILES=
@@ -18,61 +19,56 @@ else
 	COMPAT_FILES=
 endif
 
-all: vkbd-uinput
-vkbd-%:
-	mkdir -p bin
-	$(CC) $(CFLAGS) -O3 $(COMPAT_FILES) src/*.c src/vkbd/$(@:vkbd-%=%).c -o bin/keyd -lpthread $(LDFLAGS)
+all:
+	-mkdir bin
+	cp scripts/keyd-application-mapper bin/
+	$(CC) $(CFLAGS) -O3 $(COMPAT_FILES) src/*.c src/vkbd/$(VKBD).c -o bin/keyd -lpthread $(LDFLAGS)
 debug:
 	CFLAGS="-pedantic -Wall -Wextra -g" $(MAKE)
-man:
-	scdoc < keyd.md | gzip > keyd.1.gz
-	scdoc < keyd-application-mapper.md | gzip > keyd-application-mapper.1.gz
-clean:
-	-rm -rf bin
+assets:
+	for f in docs/*.scdoc; do \
+		scdoc < "$$f" | gzip > "$${f%%.scdoc}.1.gz"; \
+	done
 install:
-	mkdir -p $(DESTDIR)/etc/keyd
-	mkdir -p $(DESTDIR)$(PREFIX)/bin
-	mkdir -p $(DESTDIR)$(PREFIX)/share/man/man1
-	mkdir -p $(DESTDIR)$(PREFIX)/share/doc/keyd
-	mkdir -p $(DESTDIR)$(PREFIX)/share/doc/keyd/examples
-
 	@if [ -e $(DESTDIR)$(PREFIX)/lib/systemd/ ]; then \
-		mkdir -p $(DESTDIR)$(PREFIX)/lib/systemd/system; \
-		install -m644 keyd.service $(DESTDIR)$(PREFIX)/lib/systemd/system; \
+		install -Dm644 keyd.service $(DESTDIR)$(PREFIX)/lib/systemd/system; \
 	else \
 		echo "NOTE: systemd not found, you will need to manually add keyd to your system's init process."; \
 	fi
 
 	@if [ -e $(DESTDIR)$(PREFIX)/share/libinput/ ]; then \
-		install -m644 keyd.quirks $(DESTDIR)$(PREFIX)/share/libinput/30-keyd.quirks; \
+		install -Dm644 keyd.quirks $(DESTDIR)$(PREFIX)/share/libinput/30-keyd.quirks; \
 	else \
 		echo "WARNING: libinput not found, not installing keyd.quirks."; \
 	fi
 
+	@if [ "$(VKBD)" = "usb-gadget" ]; then \
+		install -Dm644 src/vkbd/usb-gadget.service $(DESTDIR)$(PREFIX)/lib/systemd/system/keyd-usb-gadget.service; \
+		install -Dm755 src/vkbd/usb-gadget.sh $(DESTDIR)$(PREFIX)/bin/keyd-usb-gadget.sh; \
+	fi
+
+	mkdir -p $(DESTDIR)$(PREFIX)/bin/
+	mkdir -p $(DESTDIR)$(PREFIX)/share/man/man1/
+	mkdir -p $(DESTDIR)$(PREFIX)/share/doc/keyd/
+	mkdir -p $(DESTDIR)$(PREFIX)/share/doc/keyd/examples/
 
 	-groupadd keyd
-	install -m755 bin/keyd $(DESTDIR)$(PREFIX)/bin
-	install -m755 scripts/keyd-application-mapper $(DESTDIR)$(PREFIX)/bin
-	install -m644 keyd.1.gz $(DESTDIR)$(PREFIX)/share/man/man1
-	install -m644 keyd-application-mapper.1.gz $(DESTDIR)$(PREFIX)/share/man/man1
-	install -m644 DESIGN.md CHANGELOG.md README.md $(DESTDIR)$(PREFIX)/share/doc/keyd
-	install -m644 examples/* $(DESTDIR)$(PREFIX)/share/doc/keyd/examples
+	install -m755 bin/* $(DESTDIR)$(PREFIX)/bin/
+	install -m644 docs/*.1.gz $(DESTDIR)$(PREFIX)/share/man/man1/
+	-install -m644 docs/*.md $(DESTDIR)$(PREFIX)/share/doc/keyd/
+	install -m644 examples/* $(DESTDIR)$(PREFIX)/share/doc/keyd/examples/
 
 uninstall:
-	rm -f $(DESTDIR)$(PREFIX)/share/libinput/30-keyd.quirks\
-		$(DESTDIR)$(PREFIX)/lib/systemd/system/keyd.service\
-		bin/keyd $(DESTDIR)$(PREFIX)/bin/keyd\
-		$(DESTDIR)$(PREFIX)/bin/keyd-application-mapper\
-		$(DESTDIR)$(PREFIX)/share/man/man1/keyd.1.gz\
-		$(DESTDIR)$(PREFIX)/share/man/man1/keyd-application-mapper.1.gz
-
-install-usb-gadget: install
-	install -m644 src/vkbd/usb-gadget.service $(DESTDIR)$(PREFIX)/lib/systemd/system/keyd-usb-gadget.service
-	install -m755 src/vkbd/usb-gadget.sh $(DESTDIR)$(PREFIX)/bin/keyd-usb-gadget.sh
-
-uninstall-vkbd-usb-gadget: uninstall
-	rm -f $(DESTDIR)$(PREFIX)/lib/systemd/system/keyd-usb-gadget.service\
+	rm -rf $(DESTDIR)$(PREFIX)/share/libinput/30-keyd.quirks \
+		$(DESTDIR)$(PREFIX)/lib/systemd/system/keyd.service \
+		$(DESTDIR)$(PREFIX)/bin/keyd \
+		$(DESTDIR)$(PREFIX)/bin/keyd-application-mapper \
+		$(DESTDIR)$(PREFIX)/share/doc/keyd/ \
+		$(DESTDIR)$(PREFIX)/share/man/man1/keyd*.gz \
+		$(DESTDIR)$(PREFIX)/lib/systemd/system/keyd-usb-gadget.service \
 		$(DESTDIR)$(PREFIX)/bin/keyd-usb-gadget.sh
+clean:
+	-rm -rf bin
 test: all
 	@cd t; \
 	for f in *.sh; do \

.gitignore
@@ -7,3 +7,4 @@ bin/
 *.o
 *.gch
 __pycache__
+test.log

commit 70a4211199e2a4c551e11fe4faae092cf9148e9d

Fix bug introduced in a1edfab
There was a bug introduced in a1edfab5d124aa70493dd988973f17bf4a9a4c30

https://github.com/rvaiya/keyd/blob/a1edfab5d124aa70493dd988973f17bf4a9a4c30/Makefile#L34

If `$(DESTDIR)$(PREFIX)/lib/systemd` exists, but there is no `systemd/system` directory, install will treat the command as "Install keyd.service into the file .../systemd/system" rather than "create a directory called system if it doesn't exist and install keyd.service within it". Adding the desired file name to the destination path fixes this.

Makefile
@@ -36,7 +36,7 @@ man:
 	done
 install:
 	@if [ -e $(DESTDIR)$(PREFIX)/lib/systemd/ ]; then \
-		install -Dm644 keyd.service $(DESTDIR)$(PREFIX)/lib/systemd/system; \
+		install -Dm644 keyd.service $(DESTDIR)$(PREFIX)/lib/systemd/system/keyd.service; \
 	else \
 		echo "NOTE: systemd not found, you will need to manually add keyd to your system's init process."; \
 	fi