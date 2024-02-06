2d3f879cb9c0b340857fdc9df785d1724e292141
Konstantin Vorobyev <konstantin.vorobyev@zalando.de>
2024-02-06 11:03:30 +0100

    Add layerm2() action

--- src/keyboard.c
+++ src/keyboard.c
@@ -484,6 +484,7 @@ static long process_descriptor(struct keyboard *kbd, uint8_t code,
 
 		switch (d->op) {
 		case OP_LAYERM:
+		case OP_LAYERM2:
 		case OP_ONESHOTM:
 		case OP_TOGGLEM:
 			macro = &kbd->config.macros[d->args[1].idx];
@@ -585,6 +586,7 @@ static long process_descriptor(struct keyboard *kbd, uint8_t code,
 
 		break;
 	case OP_LAYERM:
+	case OP_LAYERM2:
 	case OP_LAYER:
 		idx = d->args[0].idx;
 
@@ -791,6 +793,18 @@ static long process_descriptor(struct keyboard *kbd, uint8_t code,
 		}
 
 		break;
+	}
+	if (!pressed) {
+		struct macro *macro;
+
+		switch (d->op) {
+		case OP_LAYERM2:
+			macro = &kbd->config.macros[d->args[2].idx];
+			execute_macro(kbd, dl, macro);
+			break;
+		default:
+			break;
+		}
 	}
 
 	if (pressed)
